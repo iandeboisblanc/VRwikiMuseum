7fed6eb803cfa1e204d1abe296ff891a9223c5b9
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-05-05 14:49:04 -0700

    Add server app.js file

---
+++ server/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);  
+
+var PORT = process.env.PORT || 3000;
+
+// CORS handling
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.listen(PORT);
+console.log('Server listening on port', PORT);
+app.use(express.static(__dirname + '/client/dist'));
