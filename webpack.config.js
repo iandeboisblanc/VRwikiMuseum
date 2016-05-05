28e2eb3cd802da9b206170864b291802037af9fc
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-05-05 14:35:39 -0700

    Add webpack config and client index html and js files

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+var path = require('path');
+
+module.exports = {
+  entry: [
+    './client/index.js'
+  ],
+  output: {
+    filename: 'index_bundle.js',
+    path: __dirname + '/client/dist'
+  },
+  cache: true,
+  debug: true,
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, include: __dirname + '/client', loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [HTMLWebpackPluginConfig]
+}
