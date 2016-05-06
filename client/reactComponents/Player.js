94dd2950adfe05cd722fddfdd40f93a8a2661eed
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-05-05 21:22:28 -0700

    Remove unnecessary components from camera

--- client/reactComponents/Player.js
+++ client/reactComponents/Player.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Player = () => (
   <a-entity position='0 1.8 0'>
-    <a-entity id='player' camera look-controls wasd-controls spawner='mixin: laser; on: click' click-listener/>
+    <a-entity id='player' look-controls wasd-controls/>
   </a-entity>
 );
 
