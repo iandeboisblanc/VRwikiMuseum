558b5dc2d275f1a9348656f62db5fe53b2ecc2ae
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-05-07 18:24:33 -0700

    Remove unncessary code

--- client/index.js
+++ client/index.js
@@ -4,12 +4,9 @@ import AFRAME from 'aframe';
 
 var drawComponent = require('aframe-draw-component').component;
 AFRAME.registerComponent('draw', drawComponent);
-var htmltextureComponent = require('aframe-htmltexture-component').component;
-// AFRAME.registerComponent('htmltexture', htmltextureComponent);
-
+require('aframe-htmltexture-component')
 
 import MuseumScene from './reactComponents/MuseumScene'
-// require('aframe-htmltexture-component');
 
 ReactDOM.render(
   <div>
