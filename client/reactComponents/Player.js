2f4807601a618a90ca7ac8bce60c0f924981d8c5
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-05-08 19:59:08 -0700

    Add console logs to when player is beyond bounds

--- client/reactComponents/Player.js
+++ client/reactComponents/Player.js
@@ -3,9 +3,10 @@ import React from 'react'
 const Player = (props) => {
   var position = props.position || '0 1.8 0';
   return (
-    <a-entity position={position} 
-      limitPlayer={`xBounds:${props.xBounds}; yBounds:${props.yBounds}; zBounds:${props.zBounds}`}>
-      <a-camera id='player' look-controls wasd-controls/>
+    <a-entity position={position} >
+      <a-camera id='player' look-controls wasd-controls
+        limitPlayer={`xBounds:${props.xBounds}; yBounds:${props.yBounds}; zBounds:${props.zBounds}`}
+      />
     </a-entity>
   )
 };
@@ -20,16 +21,32 @@ AFRAME.registerComponent('limitPlayer', {
   },
 
   init: function () {
-    console.log(this);
-    console.log(this.data.xBounds, this.data.yBounds, this.data.zBounds);
-    this.camera = this.el.sceneEl.querySelector('#player');
-    //this.data.bounds;
+    // console.log(this.data.xBounds, this.data.yBounds, this.data.zBounds);
+    for(var property in this.data) {
+      if(this.data[property] !== 'undefined') {
+        this[property] = this.data[property].split(',').map((value) => Number(value));
+      }
+      // console.log(property, this[property]);
+    }
   },
 
   tick: function () {
-    var cameraPosition = this.camera.components.position.data;
-    // if(cameraPosition.x > )
-    //if camera intersects with threshhold, trigger event (link)
+    var cameraPosition = this.el.components.position.data;
+    if(this.xBounds) {
+      if(cameraPosition.x < this.xBounds[0] || cameraPosition.x > this.xBounds[1]) {
+        console.log('Out of xBounds');
+      }
+    }
+    if(this.yBounds) {
+      if(cameraPosition.y < this.yBounds[0] || cameraPosition.y > this.yBounds[1]) {
+        console.log('Out of yBounds');
+      }
+    }
+    if(this.zBounds) {
+      if(cameraPosition.z < this.zBounds[0] || cameraPosition.z > this.zBounds[1]) {
+        console.log('Out of zBounds');
+      }
+    }
   }
 })
 
