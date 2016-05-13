dc1c0496051b2c0cc7d3a2415202487c59891602
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-05-13 10:34:07 -0700

    Refactor Player as React subclass

--- client/reactComponents/Player.js
+++ client/reactComponents/Player.js
@@ -1,10 +1,27 @@
 import React from 'react'
 
-const Player = () => (
-  <a-entity position='0 1.8 0'>
-    <a-entity id='player' look-controls wasd-controls/>
-  </a-entity>
-);
+
+class Player extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    var camera = document.querySelector('#playerParent');
+    camera.addEventListener('hit', () => {
+      console.log('CAMERA REGISTERS HIT!');
+    });
+  }
+
+  render () {
+    return (
+      <a-entity id='playerParent' position='0 1.8 0'>
+        <a-camera id='player' look-controls wasd-controls/>
+      </a-entity>
+    ) 
+  }
+}
 
 module.exports = Player;
 
