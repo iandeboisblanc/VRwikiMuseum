f5e42c5b18390f7cae1ba24839dacab8361518ff
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-05-18 17:28:47 -0700

    Remove player and portal components

--- client/reactComponents/Walls.js
+++ client/reactComponents/Walls.js
@@ -20,10 +20,10 @@ class Walls extends React.Component {
         <a-box key={'linkPad' + i} 
           link={link}
           static-body
-          position={`${-this.props.width/2 + boxWidth * (i + 1) + this.doorWidth * (i + 1/2)} 0 ${-this.props.length/2}`} 
-          height='0.2'
+          position={`${-this.props.width/2 + boxWidth * (i + 1) + this.doorWidth * (i + 1/2)} 0 ${-this.props.length/2 - 0.15}`} 
+          height='0.1'
           width={this.doorWidth}
-          depth='0.01'
+          depth='0.3'
           color='grey'
           />
       )
@@ -33,10 +33,10 @@ class Walls extends React.Component {
       lowerWalls.push((
         <a-box key={'lowerWall' + i} 
           static-body
-          position={`${-this.props.width/2 + boxWidth/2 + i * (boxWidth + this.doorWidth)} 1.5 ${-this.props.length/2}`} 
+          position={`${-this.props.width/2 + boxWidth/2 + i * (boxWidth + this.doorWidth)} 1.5 ${-this.props.length/2 - 0.15}`} 
           height={this.doorHeight}
           width={boxWidth}
-          depth='0.01'
+          depth='0.3'
           color='grey'
           />
       ))
@@ -48,18 +48,18 @@ class Walls extends React.Component {
     return (
       <a-entity position='0 0 0'>
         <a-box id='northWall' 
-          static-body position={`0 ${this.wallHeight/2} ${this.props.length/2}`}
-          depth={0.01} width={this.props.width} height={this.wallHeight} color='grey'/>
+          static-body position={`0 ${this.wallHeight/2} ${this.props.length/2 + 0.15}`}
+          depth={0.3} width={this.props.width} height={this.wallHeight} color='grey'/>
         <a-box id='eastWall' 
-          static-body position={`${this.props.width/2} ${this.wallHeight/2} 0`}
-          depth={this.props.length} width={0.01} height={this.wallHeight} color='grey'/>
+          static-body position={`${this.props.width/2 + 0.15} ${this.wallHeight/2} 0`}
+          depth={this.props.length + 0.6} width={0.3} height={this.wallHeight} color='grey'/>
         <a-box id='westWall' 
-          static-body position={`${-this.props.width/2} ${this.wallHeight/2} 0`}
-          depth={this.props.length} width={0.01} height={this.wallHeight} color='grey'/>
+          static-body position={`${-this.props.width/2 - 0.15} ${this.wallHeight/2} 0`}
+          depth={this.props.length + 0.6} width={0.3} height={this.wallHeight} color='grey'/>
 
         <a-box id='southWallUpper' 
-          static-body position={`0 ${(this.wallHeight + this.doorHeight)/2} ${-this.props.length/2}`}
-          depth={0.01} width={this.props.width} height={this.wallHeight - this.doorHeight} color='grey'/>
+          static-body position={`0 ${(this.wallHeight + this.doorHeight)/2} ${-this.props.length/2 - 0.15}`}
+          depth={0.3} width={this.props.width} height={this.wallHeight - this.doorHeight} color='grey'/>
         {this.renderDoorWays.call(this)}
 
       </a-entity>
