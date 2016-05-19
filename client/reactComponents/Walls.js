c69be155f22aa1283899f91db1f2236bc7338215
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-05-19 11:22:32 -0700

    Add trim and texture to walls

--- client/reactComponents/Walls.js
+++ client/reactComponents/Walls.js
@@ -41,7 +41,17 @@ class Walls extends React.Component {
           />
       ))
     }
-    return linkPads.concat(lowerWalls);
+    let lowerWallsTrim = [];
+    for(let i = 0; i < boxCount; i++) {
+      lowerWallsTrim.push((
+        <a-box id='southWallTrim' key={'lowerWallTrim' + i}
+          position={`${-this.props.width/2 + boxWidth/2 + i * (boxWidth + this.doorWidth)} 0.1 ${-this.props.length/2}`}
+          depth={0.1} width={boxWidth - 0.001} height={0.2} 
+          material='src:#stucco; repeat:25 8;'
+          />
+      ))
+    }
+    return linkPads.concat(lowerWalls, lowerWallsTrim);
   }
 
   render () {
@@ -49,13 +59,34 @@ class Walls extends React.Component {
       <a-entity position='0 0 0'>
         <a-box id='northWall' 
           static-body position={`0 ${this.wallHeight/2} ${this.props.length/2 + 0.15}`}
-          depth={0.3} width={this.props.width} height={this.wallHeight} color='grey'/>
+          depth={0.3} width={this.props.width} height={this.wallHeight} 
+          material='src:#stucco; repeat:25 8; side:double;'
+          />
+        <a-box id='northWallTrim' 
+          position={`0 0.1 ${this.props.length/2}`}
+          depth={0.1} width={this.props.width} height={0.2} 
+          material='src:#stucco; repeat:25 8;'
+          />
         <a-box id='eastWall' 
           static-body position={`${this.props.width/2 + 0.15} ${this.wallHeight/2} 0`}
-          depth={this.props.length + 0.6} width={0.3} height={this.wallHeight} color='grey'/>
+          depth={this.props.length + 0.6} width={0.3} height={this.wallHeight} 
+          material='src:#stucco2; repeat:25 8;'
+          />
+        <a-box id='eastWallTrim' 
+          position={`${this.props.width/2} 0.1 0`}
+          depth={this.props.length} width={0.1} height={0.2} 
+          material='src:#stucco; repeat:25 8;'
+          />
         <a-box id='westWall' 
           static-body position={`${-this.props.width/2 - 0.15} ${this.wallHeight/2} 0`}
-          depth={this.props.length + 0.6} width={0.3} height={this.wallHeight} color='grey'/>
+          depth={this.props.length + 0.6} width={0.3} height={this.wallHeight} 
+          material='src:#marbleSurface; repeat:25 8;'
+          />
+        <a-box id='westWallTrim' 
+          position={`${-this.props.width/2} 0.1 0`}
+          depth={this.props.length} width={0.1} height={0.2} 
+          material='src:#stucco; repeat:25 8;'
+          />
 
         <a-box id='southWallUpper' 
           static-body position={`0 ${(this.wallHeight + this.doorHeight)/2} ${-this.props.length/2 - 0.15}`}
