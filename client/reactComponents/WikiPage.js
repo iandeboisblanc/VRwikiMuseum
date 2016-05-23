f54cfa5e9cfe5b07405341f4edb8a4b66b380f91
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-05-23 14:24:12 -0700

    Remove images state in favor of .thumb check--and end content at occurence of see Also section

--- client/reactComponents/WikiPage.js
+++ client/reactComponents/WikiPage.js
@@ -29,39 +29,43 @@ class WikiPage extends React.Component {
         success: (data, textStatus, jqXHR) => {
           // Parse Wiki data into discrete sections by topic
           let rawResults = $('<div id="rawResults"/>').append(data.parse.text["*"])[0];
-          let images = $(rawResults).find('img').map((index, element) => {
-            if($(element).attr('width') > 100) {
-              return $('<section />').append(element)
-            }
-          });
-          let filteredResults = $(rawResults).children('p, h2, h3, table');
+          // let images = $(rawResults).find('img').map((index, element) => {
+          //   if($(element).attr('width') > 100) {
+          //     return $('<section />').append(element)
+          //   }
+          // });
+          let filteredResults = $(rawResults).children('p, h2, h3, table, .thumb');
           filteredResults = filteredResults.filter((child, element) => element.innerText.length > 0);
 
           const parsedHtmlSections = [];
+          let contentEnded = false;
           for(var i = 0; i < filteredResults.length; i++) {
             let htmlSection = filteredResults[i]
-            $(htmlSection).css('padding', '0px 10px');
-            $(htmlSection).children('.mw-editsection').empty(); //Remove 'Edit' tags on titles
+            if($(htmlSection).children('#See_also, #References, #External_links').length) {
+              contentEnded = true;
+            }
+            if(!contentEnded) {
+              $(htmlSection).css('padding', '0px 10px');
+              $(htmlSection).children('.mw-editsection').empty(); //Remove 'Edit' tags on titles
 
-            // If header that is not See Also, References, or External Links, create a new Section
-            // Otherwise, add to previous section
-            if($(htmlSection).is('h2')) {
-              if($(htmlSection).children('#See_also, #References, #External_links').length == 0) {
+              // If header that is not See Also, References, or External Links, create a new Section
+              // Otherwise, add to previous section
+              if($(htmlSection).is('h2')) {
                 var newSection = $('<section />').append(htmlSection);
                 parsedHtmlSections.push(newSection);
-              }
-            } else {
-              if(parsedHtmlSections.length) {
-                $(parsedHtmlSections[parsedHtmlSections.length - 1]).append(htmlSection)
               } else {
-                var newSection = $('<section/>').append(htmlSection);
-                parsedHtmlSections.push(newSection);
+                if(parsedHtmlSections.length) {
+                  $(parsedHtmlSections[parsedHtmlSections.length - 1]).append(htmlSection)
+                } else {
+                  var newSection = $('<section/>').append(htmlSection);
+                  parsedHtmlSections.push(newSection);
+                }
               }
             }
           }
           this.setState({ 
             textDisplayHtml: parsedHtmlSections,
-            images: images,
+            // images: images,
             infoLoaded: true
           });
         },
